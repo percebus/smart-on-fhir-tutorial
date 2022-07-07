e8672729510b82e5883c2df56a24d715d29150c7
JC Guerrero <JCystems@gMail.com>
2022-07-07 16:10:49 -0500

    tweaks

--- app/cfg/logica.config.js
+++ app/cfg/logica.config.js
@@ -1,5 +1,2 @@
-(() => {
-  'use strict'
-  window.CLIENT_ID = '433d927a-9e75-4ac9-bff8-9cd691a8ffcd'
-  window.SMART_HEALTH_IT_CLIENT_ID = null
-})()
+CLIENT_ID = '433d927a-9e75-4ac9-bff8-9cd691a8ffcd'
+SMART_HEALTH_IT_CLIENT_ID = null
